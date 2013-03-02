8aa64720ea99aa3ab066e1b03a75d2be15f01282
Jimmy D. Burrell <jimmydburr@gmail.com>
2013-03-02 15:43:47 -0600

    added client.html and socket.io

--- partyline.js
+++ partyline.js
@@ -1,4 +1,15 @@
-var = require('http');
+var http = require('http'),
+	fs = require('fs');
 
-
+var srv = http.createServer(function(req, res) {
+	fs.readFile(__dirname + '/client.html', function (err, data){
+		if (err) {
+			console.log ('Error: ' + err);
+			res.writeHead(500);
+			return res.end('Error loading client.html');
+		}
+	res.writeHead(200);
+	res.end(data);
+	});
+}).listen(8080);
 
