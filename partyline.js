b9efa3b02d1d072bc06d52c7ce316139ce060509
Jimmy D. Burrell <jimmydburr@gmail.com>
2013-03-02 16:56:31 -0600

    expanded both client and server code

--- partyline.js
+++ partyline.js
@@ -1,15 +1,30 @@
-var http = require('http'),
-	fs = require('fs');
+var app = require('http').createServer(handler),
+	fs = require('fs'),
+	io = require('socket.io').listen(app);
+app.listen(8080);
 
-var srv = http.createServer(function(req, res) {
+function handler (req, res) {
+	// upon first connect send the client code
 	fs.readFile(__dirname + '/client.html', function (err, data){
 		if (err) {
-			console.log ('Error: ' + err);
+			console.log (err);
 			res.writeHead(500);
 			return res.end('Error loading client.html');
 		}
 	res.writeHead(200);
 	res.end(data);
 	});
-}).listen(8080);
+}
+// create a websocket
+io.sockets.on('connection', function(socket) {
+		socket.emit('chat', {Chatting: 'on the partyline'});
+		socket.on('set nickname', function(nickname){
+			socket.set('nickname', nickname, function (){
+				var connected_msg = nickname + ' is now connected.';
+				console.log(connected_msg);
+				io.sockets.volatile.emit('broadcast_msg', connected_msg);
+			});
+		});
+});
+
 
