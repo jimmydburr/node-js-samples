00c283e996d0144e9c08954d073f406bd94ac406
Jimmy D. Burrell <jimmydburr@gmail.com>
2013-03-03 15:06:43 -0600

    customized mysql query to web_manager

--- node2mysql.js
+++ node2mysql.js
@@ -1,12 +1,10 @@
 var db = require("mysql-native").createTCPClient('dev'); // localhost:3306 by default
 db.auto_prepare = true;
-function dump_rows(cmd)
-{
+function dump_rows(cmd) {
 	cmd.addListener('row', function(r) { console.dir(r); } );
 }
 
 db.auth("web_manager", "root", "fmp234");
-dump_rows(db.query("select 1+1,2,3,'4',length('hello')"));
-dump_rows(db.execute("select 1+1,2,3,'4',length(?)", ["hello"]));
+dump_rows(db.query("select id,email from app order by id desc limit 150"));
 db.close();
 
