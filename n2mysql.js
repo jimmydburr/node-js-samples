5a937976a1b12daba79ba283af0575d1084e2911
Jimmy D. Burrell <jimmydburr@gmail.com>
2013-03-04 06:10:31 -0600

    n2mysql.js

--- n2mysql.js
+++ n2mysql.js
@@ -8,9 +8,13 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT id,email from app order by id desc limit 150', function(err, rows, fields) {
+connection.query('SELECT id,email from app order by id desc limit 15', function(err, rows, fields) {
 	if (err) throw err;
 	console.log(rows);
+	rows.forEach(function(col,index,a) {
+		console.log(col.id);
+		console.log(col.email);
+	});
 });
 
 connection.end();
