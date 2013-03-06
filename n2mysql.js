1990e5a48c3885af5eb39e7362a789fea670f115
Jimmy D. Burrell <jimmydburr@gmail.com>
2013-03-05 20:00:40 -0600

    tweaked

--- n2mysql.js
+++ n2mysql.js
@@ -11,7 +11,6 @@ connection.connect();
 connection.query('SELECT id,email from app order by id desc limit 15', function(err, rows, fields) {
 	if (err) throw err;
 	console.log(rows);
-	console.log(fields);
 	rows.forEach(function(col,index,a) {
 		console.log(col.id);
 		console.log(col.email);
