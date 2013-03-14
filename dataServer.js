a1e8cea87d214308032eea197813f11a2b223e48
Jimmy D. Burrell <jimmydburr@gmail.com>
2013-03-13 23:01:10 -0500

    fixed bugs in jsgdyn and got socket data Xfer working

--- dataServer.js
+++ dataServer.js
@@ -1,8 +1,8 @@
 var os=require('os');
 var myData = new Object;
-myData.cpu = os.cpus()[0].times.user/(1024*1024);
-myData.memory = os.freemem()/(1024*1024*100);
-//console.log(myData);
+myData.cpu = os.cpus()[0].times.user;
+myData.memory = os.freemem();
+console.log(myData);
 //throw '';
 
 var app = require('http').createServer(handler),
@@ -12,11 +12,11 @@ app.listen(8080);
 
 function handler (req, res) {
 	// upon first connect send the client code
-	fs.readFile(__dirname + '/client.html', function (err, data){
+	fs.readFile(__dirname + '/jsgdyn.html', function (err, data){
 		if (err) {
 			console.log (err);
 			res.writeHead(500);
-			return res.end('Error loading client.html');
+			return res.end('Error loading jsgdyn.html');
 		}
 	res.writeHead(200);
 	res.end(data);
@@ -24,7 +24,16 @@ function handler (req, res) {
 }
 // create a websocket
 io.sockets.on('connection', function(socket) {
+	console.log('Got a connection.');
+	myData.cpu = os.cpus()[0].times.user/(1024*1024);
+	myData.memory = os.freemem()/(1024*1024*100);
 	socket.emit('broadcast_msg', myData);
-});
 
+	setInterval(function() {
+		myData.cpu = os.cpus()[0].times.user/(1024*1024);
+		myData.memory = os.freemem()/(1024*1024*100);
+		io.sockets.volatile.emit('broadcast_msg', myData);
+	}, 1000);
+
+});
 
