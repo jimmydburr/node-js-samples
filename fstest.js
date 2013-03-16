7b3737c4b1367b7b457c4abdc20125bf551f9b31
Jimmy D. Burrell <jimmydburr@gmail.com>
2013-03-15 20:14:28 -0500

    added use strict

--- fstest.js
+++ fstest.js
@@ -1,27 +1,25 @@
+"use strict";
 var fs = require('fs');
 var myfiles = [];
 
 var files = fs.readdir('/tmp', function(err, files) {
-  if (err) throw err;
-  console.log('> Reading files');
-  myfiles = files;
+	if (err) {throw err; }
+	console.log('> Reading files');
+	myfiles = files;
 });
 
 var server = require('http').createServer();
 
 server.on("request", function(request, response) {
 	console.log("> Request Started");
-	 request.on("end", function() {
-		response.writeHead(200, { 'Content-Type': 'text/plain' })
-		myfiles.forEach(function(element, index, array){
-			response.write(index + ' ' + element.toString() + "\n")
+	request.on("end", function() {
+		response.writeHead(200, { 'Content-Type': 'text/plain' });
+		myfiles.forEach(function(element, index, array) {
+			response.write(index + ' ' + element.toString() + "\n");
 		});
 		response.end("Done!");
 		console.log("> Request closed");
-	})
+	});
 });
 server.listen(8080);
-
-
-
 
